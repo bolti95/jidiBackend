a9d77e7676a4106bdd8238786d62ac54d6284263
Diwa Wardak <diwawardak1@gmail.com>
2020-11-02 14:24:30 +0000

    basket router added

---
+++ routes/basketRouter.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+
+router.get('/basket', async (req, res) => {
+    res.send('Items in Basket');
+});
+
+router.put('update', async (req, res) => {
+    res.send('Order updated')
+});
+
+router.delete('/delete', async(req, res) => {
+    res.send('Items deleted')
+})
+
+module.exports = router;
