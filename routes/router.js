e95f7a41be6f0f5ec993e0e44b2618caa66c1672
Diwa Wardak <69535939+Diwawardak@users.noreply.github.com>
2020-11-02 14:16:38 +0000

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -2,9 +2,5 @@ const router = require("express").Router();
 
 router.get('/', (req, res) => {
     res.send('Home Page');
-<<<<<<< HEAD
-});
-=======
 });
 
->>>>>>> c67d73482f996d2aae0b39c2aa050794cd0ca25e
