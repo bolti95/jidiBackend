b86b4e2a3ebd6e25346a38ae8f1fd3818ae22523
Izzy Bolt <izzy@VIV-ID-MBA-04.local>
2020-11-11 13:22:58 +0000

    updated with checkout router, ID created

--- routes/checkoutRouter.js
+++ routes/checkoutRouter.js
@@ -1,12 +1,24 @@
 const router = require("express").Router();
+const orderid = require('order-id')('mysecret');
+const bodyParser = require('body-parser');
 
 router.get('/', async (req, res) => {
     res.send('Checkout page');
 });
 
 router.post('/create', async (req, res) => {
-    res.send('Order created')
-});
+        let {customerName, email, cardNumber, expiryDate, cvc} = req.body 
+        const orderNumber = orderid.generate();
+        
+        // orderid.getTime(id)
+    
+       
+        console.log(req.body)
+        res.send({message: 'Order created', ID: orderNumber})
+       
+    });
+    
+
 
 router.delete('/delete', async(req, res) => {
     res.send('Order now deleted')
