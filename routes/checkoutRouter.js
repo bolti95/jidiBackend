5540720f83a2c21ae4af9ed5b93bbaeff0cdef40
Izzy Bolt <izzy@VIV-ID-MBA-04.local>
2020-11-11 10:51:16 +0000

    most up to date

--- routes/checkoutRouter.js
+++ routes/checkoutRouter.js
@@ -1,20 +1,22 @@
 const router = require("express").Router();
 const bodyParser = require('body-parser');
 const functions = require('../lib/connection');
+const orderid = require('order-id')('mysecret');
 
 
 router.get('/', async (req, res) => {
     res.send('Checkout page');
 });
 
-router.get('/create', async (req, res) => {
-    res.send('Checkout page');
-});
 
 router.post('/create', async (req, res) => {
-    let {customerName, Items, SaleAmount, email, cardNumber} = req.body
+    let {customerName, Items, SaleAmount, email, cardNumber} = req.body 
+    const ID = orderid.generate();
+    
+    orderid.getTime(id)
 
     let customerOrder = ({
+        ID,
         customerName,
         Items, 
         SaleAmount,
@@ -22,8 +24,13 @@ router.post('/create', async (req, res) => {
         cardNumber      
     }) 
     res.send('Order created', req.body)
+    console.log(customerOrder)
     console.log(req.body)
     //if statement: if the data is there, create order number, if it's not, create error
+});
+
+router.get('/create', async (req, res) => {
+    res.send('Checkout page 123');
 });
 
 router.delete('/delete', async(req, res) => {
